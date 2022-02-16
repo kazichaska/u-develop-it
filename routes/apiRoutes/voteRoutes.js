cc2c5425a0da1e6cd2d6e3fd918439b23af84f7b
Kazi Islam <kazichaska@gmail.com>
2022-02-15 19:34:09 -0600

    votes API routes created and tested

---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const res = require('express/lib/response');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+router.get('/votes', (req, res) => {
+    const sql = `select candidates.*, parties.name AS party_name, 
+    count(candidate_id) as count from votes 
+    left join candidates ON votes.candidate_id = candidates.id 
+    left join parties on candidates.party_id = parties.id 
+    group by candidate_id order by count desc`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+router.post('/vote', ({ body }, res) => {
+    // data validation
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `insert into votes (voter_id, candidate_id) values (?,?)`
+    const params = [body.voter_id, body.candidate_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    });
+});
+
+module.exports = router;
